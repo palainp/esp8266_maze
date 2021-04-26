217a8107a4a08dcad6562f181810ffe1a10d8872
palainp <pierre.alain@gmail.com>
2021-04-26 11:54:59 +0200

    revert bad search/replace

--- displayer/resolver.c
+++ displayer/resolver.c
@@ -146,7 +146,7 @@ int main(int argc, char ** argv)
     {
         const Uint32 format = SDL_PIXELFORMAT_ARGB8888;
         SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormat(0, 800, 800, 32, format);
-        SDL_RenderReadPxels(renderer, NULL, format, surface->pixels, surface->pitch);
+        SDL_RenderReadPixels(renderer, NULL, format, surface->pixels, surface->pitch);
         SDL_SaveBMP(surface, "screenshot.bmp");
         SDL_FreeSurface(surface);
         printf("path found in %d moves\n", current->gscore);
